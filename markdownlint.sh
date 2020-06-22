6d62ef0f2128e906f2c369618a62f595cdec4065
Christian Dupuis <cd@atomist.com>
2020-06-22 18:01:47 +0200

    Bail out early on a markdownlint branch

--- markdownlint.sh
+++ markdownlint.sh
@@ -10,13 +10,20 @@ ignores=$( cat $ATOMIST_PAYLOAD |
   jq -r '.skill.configuration.instances[0].parameters[] | select( .name == "ignores" ) | .value | join(" --ignore ")' )
 labels=$( cat $ATOMIST_PAYLOAD |
   jq -r '.skill.configuration.instances[0].parameters[] | select( .name == "labels" ) | .value' )
+branch=$( cat $ATOMIST_PAYLOAD |
+  jq -r '.data.Push[0].branch' )
+
+##### Bail out early if it on a markdownlint branch
+if [[ "$branch" =~ ^markdownlint-.* ]]
+then
+    exit 0
+fi
 
 ##### Make the problem matcher available to the runtime
 cp /app/markdownlint.matcher.json /atm/output/matchers/
 
 ##### Create push instructions for the runtime to indicate how changes to the repo should get persisted
-if [ -z "$labels" ]
-then
+if [[ -z "$labels" ]]; then
   labels="[]"
 fi
 push=$( jq -n \
@@ -42,19 +49,16 @@ push=$( jq -n \
 echo $push > /atm/output/push.json
 
 ##### Prepare command arguments
-if ! [ -z "$fix" ]
-then
+if ! [[ -z "$fix" ]]; then
   fix_option="--fix"
 fi
 
-if ! [ -z "$config" ] && ! [ -f "/atm/home/.markdownlint.json" ]
-then
+if ! [[ -z "$config" ]] && ! [[ -f "/atm/home/.markdownlint.json" ]]; then
   echo $config > "/atm/input/markdownlint.config.json"
   config_option="--config /atm/input/markdownlint.config.json"
 fi
 
-if ! [ -z "ignores" ]
-then
+if ! [[ -z "ignores" ]]; then
   ignore_option="--ignore $ignores"
 fi
 
